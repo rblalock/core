e445ebc8c2c6148910a7c2e67d46f2fb1992c632
grantges <bert@grantges.co>
2014-03-27 08:35:24 -0500

    Update core.js
    Update to the "core" of core to include a Navigator Object. openScreen function, and all subsequent navigation would be part of the core object, so that its more easily overridden when using widgets or other custom navigation patterns.
    
    Additionally added a `_modal` flag to the openScreen function to account for modal windows within the application.

--- app/lib/core.js
+++ app/lib/core.js
@@ -57,56 +57,68 @@ var App = {
 		App.getDeviceDimensions();
 	},
 	/**
-	 * Handle cross platform way of opening a main screen.
-	 *
-	 * See example:
-	 *
-	 * 		var login = Alloy.createController("login");
-	 * 		App.openScreen(login);
-	 *
-	 * or if you don't need the controller reference:
-	 *
-	 * 		App.openScreen("login");
-	 *
-	 * This is a simplified, app-wide navigation
-	 * example which auto-adds a window to the controller.
-	 * More could be added here depending on the navigation requirements.
-	 * For instance, if you have a navgroup for handheld, you could open it in that
-	 * for iOS and differently in Android, etc.  The point is, managing your
-	 * app navigation flow in one spot is a good idea.
-	 *
-	 * @param {Controllers/String} _controller
-	 * @param {Object} _controllerArguments The arguments for the controller (optional)
-	 * @return {Controllers} Returns the new controller
+	 * Navigator
+	 * @Object
+	 * 
+	 * Class to handle all navigation aspects of the application. To override the navigation in the main CoreJS, 
+	 * you can easily set the navigator object to an alternate class, widget etc. 
+	 * 
 	 */
-	openScreen: function(_controller, _controllerArguments) {
-		var controller = null;
+	Navigator: {
 		
-		if(typeof _controller === "string") {
-			controller = Alloy.createController(_controller, _controllerArguments);
-		} else {
-			controller = _controller;
+		/** Handle cross platform way of opening a main screen.
+		 *
+		 * See example:
+		 *
+		 * 		var login = Alloy.createController("login");
+		 * 		App.Navigator.openScreen(login);
+		 *
+		 * or if you don't need the controller reference:
+		 *
+		 * 		App.openScreen("login");
+		 *
+		 * This is a simplified, app-wide navigation
+		 * example which auto-adds a window to the controller.
+		 * More could be added here depending on the navigation requirements.
+		 * For instance, if you have a navgroup for handheld, you could open it in that
+		 * for iOS and differently in Android, etc.  The point is, managing your
+		 * app navigation flow in one spot is a good idea.
+		 *
+		 * @param {Controllers/String} _controller
+		 * @param {Object} _controllerArguments The arguments for the controller (optional)
+		 * @param {Boolean} _modal Set to true if you want this to be a modal window
+		 * @return {Controllers} Returns the new controller
+		 */
+		openScreen: function(_controller, _controllerArguments, _modal) {
+			var controller = null;
+			
+			if(typeof _controller === "string") {
+				controller = Alloy.createController(_controller, _controllerArguments);
+			} else {
+				controller = _controller;
+			}
+	
+			// In this example we add a window to a controller
+			// opened via `openScreen`. Keeping windows out of the
+			// UI architecture keep the Views flexible.
+			controller.window = Ti.UI.createWindow({
+				backgroundColor: Alloy.CFG.windowBackgroundColor,
+				statusBarStyle: OS_IOS ? Ti.UI.iPhone.StatusBar.LIGHT_CONTENT : null,
+				modal: _modal || false
+			});
+			
+			controller.window.add(controller.wrapper);
+	
+			// Set views for current orientation
+			App.setViewsForOrientation(controller);
+	
+			// Bind orientation events for this controller instance
+			App.bindOrientationEvents(controller);
+	
+			controller.window.open();
+	
+			return controller;
 		}
-
-		// In this example we add a window to a controller
-		// opened via `openScreen`. Keeping windows out of the
-		// UI architecture keep the Views flexible.
-		controller.window = Ti.UI.createWindow({
-			backgroundColor: Alloy.CFG.windowBackgroundColor,
-			statusBarStyle: OS_IOS ? Ti.UI.iPhone.StatusBar.LIGHT_CONTENT : null
-		});
-		
-		controller.window.add(controller.wrapper);
-
-		// Set views for current orientation
-		App.setViewsForOrientation(controller);
-
-		// Bind orientation events for this controller instance
-		App.bindOrientationEvents(controller);
-
-		controller.window.open();
-
-		return controller;
 	},
 	/**
 	 * Helper to bind the orientation events to a controller. These get added automatically
