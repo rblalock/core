88ace58466ed721bff459c363190765195c43f1b
rblalock <rickblalock@mac.com>
2014-04-14 22:48:14 -0400

    Added unit test for standardNavigation Example

--- demo_app/app/lib/tests/core.js
+++ demo_app/app/lib/tests/core.js
@@ -11,97 +11,22 @@ mocha.setup({ reporter: 'ti-spec-studio' });
 // Navigation tests
 describe("navigation.js", function () {
 	// Open controller test
-	it("open(); Opens a controller via a string", function () {
-		var window = Ti.UI.createWindow({ backgroundColor: "#eee" });
-		window.open();
-
-		// Require in the navigation module
-		var navigator = require("navigation")({
-			parent: window
-		});
-
-		navigator.open("screen");
-
-		if(navigator.controllers.length < 1) {
-			throw new Error();
+	it("open(); Opens a controller / window via a string", function () {
+		if(OS_IOS) {
+			var navWindow = Ti.UI.iOS.createNavigationWindow({
+				window: Ti.UI.createWindow()
+			});
+			navWindow.open();
 		}
-	});
-
-	// Close controller test
-	it("close(); Closes a controller", function (done) {
-		var window = Ti.UI.createWindow({ backgroundColor: "#eee" });
-		window.open();
 
 		// Require in the navigation module
 		var navigator = require("navigation")({
-			parent: window
+			parent: navWindow || null
 		});
 
-		navigator.open("screen");
+		var controller = navigator.open("screen");
 
-		// Make sure the navigator isn't busy, then run the test
-		var interval = setInterval(function() {
-			if(!navigator.isBusy) {
-				navigator.close(function() {
-					if(navigator.controllers.length > 0) {
-						throw new Error();
-					}
-
-					done();
-				});
-
-				clearInterval(interval);
-			}
-		}, 100);
-	});
-
-	// Go to home controller text
-	it("closeToHome(); Closes all but first controller", function (done) {
-		var window = Ti.UI.createWindow({ backgroundColor: "#eee" });
-		window.open();
-
-		// Require in the navigation module
-		var navigator = require("navigation")({
-			parent: window
-		});
-
-		navigator.open("screen");
-		navigator.open("screen");
-
-		// Make sure the navigator isn't busy, then run the test
-		var interval = setInterval(function() {
-			if(!navigator.isBusy) {
-				navigator.closeToHome(function() {
-					if(navigator.controllers.length !== 1) {
-						throw new Error();
-					}
-
-					done();
-				});
-
-				clearInterval(interval);
-			}
-		}, 100);
-	});
-
-	// Close all controllers
-	it("closeAll(); Closes all controllers", function () {
-		var window = Ti.UI.createWindow({ backgroundColor: "#eee" });
-		window.open();
-
-		// Require in the navigation module
-		var navigator = require("navigation")({
-			parent: window
-		});
-
-		navigator.open("screen");
-		navigator.open("screen");
-		navigator.open("screen");
-		navigator.open("screen");
-
-		navigator.closeAll();
-
-		if(navigator.controllers.length > 0) {
+		if(!controller.window) {
 			throw new Error();
 		}
 	});
